8f06b78960a99adcec4e853eb89646fbf175d244
Saurabh Jain <100847303+jainsaurabh033@users.noreply.github.com>
2026-05-22 03:42:14 +0530

    Time: 284 ms (85.59%), Space: 223.4 MB (38.54%) - LeetHub

--- 3934-smallest-unique-subarray/3934-smallest-unique-subarray.java
+++ 3934-smallest-unique-subarray/3934-smallest-unique-subarray.java
@@ -1,44 +1,41 @@
 class Solution {
     long BASE = 10;
     long MOD = 100000007L;
+
     public int smallestUniqueSubarray(int[] nums) {
-        int n = nums.length;
+       int n = nums.length;
 
-        long[] pow = new long[n+1];
-        long[] hash = new long[n+1];
-        pow[0] = 1;
+       long[] pow = new long[n+1];
+       long[] hash = new long[n+1];
 
-        for(int i = 1;i<=n;i++){
-            pow[i] = (pow[i-1] * BASE) % MOD;
-        }
+       pow[0] = 1;
 
-        for(int i = 0;i<n;i++){
-            hash[i+1] = (hash[i] * BASE + nums[i]) % MOD;
-        }
+       for(int i = 1;i<=n;i++){
+          pow[i] = (pow[i-1] * BASE) % MOD;
+       }    
 
-        for(int i = 0;i<=n;i++){
-            System.out.print(hash[i] + " ");
-        }
-        System.out.println();
+       for(int i = 0;i<n;i++){
+          hash[i+1] = (hash[i] * BASE + nums[i]) % MOD;
+       }
 
-        int low = 1;
-        int high = n;
+       int low = 1;
+       int high = n;
 
-        int ans = n;
+       int ans = n;
 
-        while(low <= high){
-            int mid = low + (high - low) / 2;
+       while(low <= high){
+         int mid = low + (high - low) / 2;
 
-            if(check(nums, mid, hash, pow)){
-                ans = mid;
-                high = mid - 1;
-            }
-            else{
-                low = mid + 1;
-            }
-        }
+         if(check(nums, mid, hash, pow)){
+            ans = mid;
+            high = mid - 1;
+         }
+         else{
+            low = mid + 1;
+         }
+       }
 
-        return ans;
+       return ans;
     }
 
     private boolean check(int[] nums, int len, long[] hash, long[] pow){
@@ -46,7 +43,7 @@ class Solution {
 
         Map<Long, Integer> freq = new HashMap<>();
 
-        for(int i = 0;i + len<=n;i++){
+        for(int i = 0;i+len <= n;i++){
             int l = i;
             int r = i + len - 1;
 
@@ -64,9 +61,8 @@ class Solution {
         return false;
     }
 
-    private long getHash(int l, int r , long[] hash, long[] pow){
+    private long getHash(int l, int r, long[] hash, long[] pow){
         long result = (hash[r+1] - (hash[l] * pow[r-l+1]) % MOD + MOD) % MOD;
-
         return result;
     }
 }
