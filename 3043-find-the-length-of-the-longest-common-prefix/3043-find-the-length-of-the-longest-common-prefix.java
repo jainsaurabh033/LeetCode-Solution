f4bad007049baea3e3c5ace5f5fe49a1d324a3cc
Saurabh Jain <100847303+jainsaurabh033@users.noreply.github.com>
2026-05-21 13:53:43 +0530

    Time: 220 ms (16.79%), Space: 90.3 MB (23.98%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -4,24 +4,23 @@ class Solution {
         int m = arr2.length;
 
         Set<String> st = new HashSet<>();
+        StringBuilder temp = new StringBuilder();
 
         for(int i = 0;i<n;i++){
             String s = Integer.toString(arr1[i]);
-
-            StringBuilder temp = new StringBuilder();
 
             for(int j = 0;j<s.length();j++){
                 temp.append(s.charAt(j));
                 st.add(temp.toString());
             }
+
+            temp.setLength(0);
         }
 
         int ans = 0;
 
         for(int i = 0;i<m;i++){
             String s = Integer.toString(arr2[i]);
-
-            StringBuilder temp = new StringBuilder();
 
             for(int j = 0;j<s.length();j++){
                 temp.append(s.charAt(j));
@@ -30,6 +29,8 @@ class Solution {
                     ans = Math.max(ans, temp.length());
                 }
             }
+
+            temp.setLength(0);
         }
 
         return ans;
