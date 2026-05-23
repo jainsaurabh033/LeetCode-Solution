221bbfb9ea83577068b9d5db6273af6c14f924ce
Saurabh Jain <100847303+jainsaurabh033@users.noreply.github.com>
2026-05-23 22:45:22 +0530

    Time: 296 ms (11.48%), Space: 45.1 MB (50.19%) - LeetHub

---
+++ 3937-minimum-operations-to-make-array-modulo-alternating-i/3937-minimum-operations-to-make-array-modulo-alternating-i.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int n = nums.length;
+
+        int ans = Integer.MAX_VALUE;
+
+        for(int x = 1;x<=k;x++){
+            for(int y = 1;y<=k;y++){
+                if(x == y) continue;
+
+                int count = 0;
+
+                for(int i = 0;i<n;i++){
+                    int rem = nums[i] % k;
+
+                    if(i % 2 == 0){
+                        int front = (x - rem + k) % k;
+                        int back = (rem - x + k) % k;
+
+                        count += Math.min(front,back);
+                    }
+                    else{
+                        int front = (y - rem + k) % k;
+                        int back = (rem - y + k) % k;
+
+                        count += Math.min(front,back);
+                    }
+                }
+
+                ans = Math.min(ans, count);
+            }
+        }
+
+        return ans;
+    }
+}
