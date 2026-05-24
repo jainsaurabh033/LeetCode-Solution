8908b19e8bf4fdb6c0bb054b0ad1370209c3eded
Saurabh Jain <100847303+jainsaurabh033@users.noreply.github.com>
2026-05-24 12:03:45 +0530

    Time: 20 ms (14.01%), Space: 47.3 MB (37.81%) - LeetHub

---
+++ 3941-password-strength/3941-password-strength.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int passwordStrength(String password) {
+        int n = password.length();
+
+        Map<Character, Boolean> map = new HashMap<>();
+
+        int ans = 0;
+
+        for(int i = 0;i<n;i++){
+            char c = password.charAt(i);
+
+            if(map.getOrDefault(c, false) == false){
+                if(isLowerCaseChar(c)){
+                    ans += 1;
+                }
+                else if(isUpperCaseChar(c)){
+                    ans+= 2;
+                }
+                else if(isNumber(c)){
+                    ans += 3;
+                }
+                else if(isSpecialChar(c)){
+                    ans += 5;
+                }
+            }
+
+            map.put(c, true);
+        }
+
+        return ans;
+    }
+
+    private boolean isLowerCaseChar(char c){
+        if('a' <= c && c <= 'z') return true;
+
+        return false;
+    }
+
+    private boolean isUpperCaseChar(char c){
+        if('A' <= c && c <= 'Z') return true;
+
+        return false;
+    }
+
+    private boolean isNumber (char c){
+        if('0' <= c && c <= '9') return true;
+
+        return false;
+    }
+
+    private boolean isSpecialChar(char c){
+        if(c == '!' || c == '@' || c == '#' || c == '$') return true;
+
+        return false;
+    }
+}
